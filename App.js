bf3dc92dac5a6bb5dd2ad8f5196076ffc3c39160
KaijunChen0 <113864900+KaijunChen0@users.noreply.github.com>
2024-04-21 09:42:03 -0700

    update App.js setting

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ import "dotenv/config";
 
 // const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 const CONNECTION_STRING =
-  "mongodb://127.0.0.1:27017/kanbas" || process.env.DB_CONNECTION_STRING;
+  "mongodb://127.0.0.1:27017/newkanbas" || process.env.DB_CONNECTION_STRING;
 
 mongoose.connect(CONNECTION_STRING);
 
